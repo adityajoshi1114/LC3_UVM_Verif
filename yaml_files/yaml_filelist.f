89541c41ca84167e74bcf50137cddab57d15490b
Aleesha Jiwani <102981898+alee0104@users.noreply.github.com>
2024-11-10 08:34:45 -0700

    Add files via upload

---
+++ yaml_files/yaml_filelist.f
@@ -0,0 +1,5 @@
+/mnt/ncsudrive/a/ajiwani/748/Project3/mem_yaml_files/memaccess_in_if.yml
+/mnt/ncsudrive/a/ajiwani/748/Project3/mem_yaml_files/memaccess_out_if.yml
+/mnt/ncsudrive/a/ajiwani/748/Project3/mem_yaml_files/memaccess_env_if.yml
+
+
