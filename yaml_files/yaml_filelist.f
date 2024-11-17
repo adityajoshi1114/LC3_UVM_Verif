5a89ef08999ff5bf4cb128df6c9fa67ad17bf9af
Aditya Joshi <ajoshi25@grendel32.ece.ncsu.edu>
2024-11-16 20:59:16 -0500

    Fixed names for memaccess

--- yaml_files/yaml_filelist.f
+++
@@ -1,5 +0,0 @@
-/mnt/ncsudrive/a/ajiwani/748/Project3/mem_yaml_files/memaccess_in_if.yml
-/mnt/ncsudrive/a/ajiwani/748/Project3/mem_yaml_files/memaccess_out_if.yml
-/mnt/ncsudrive/a/ajiwani/748/Project3/mem_yaml_files/memaccess_env_if.yml
-
-
