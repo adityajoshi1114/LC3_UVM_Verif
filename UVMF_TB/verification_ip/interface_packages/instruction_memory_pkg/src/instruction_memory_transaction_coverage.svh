7c4e1abb9524553bc1e55dc7b1ce0047b6f07cb5
Thribhuvan Reddy Kanna Reddy <trkannar@grendel33.ece.ncsu.edu>
2024-11-26 18:58:19 -0500

    Updated Coverage Model

--- UVMF_TB/verification_ip/interface_packages/instruction_memory_pkg/src/instruction_memory_transaction_coverage.svh
+++ UVMF_TB/verification_ip/interface_packages/instruction_memory_pkg/src/instruction_memory_transaction_coverage.svh
@@ -1,4 +1,4 @@
-//----------------------------------------------------------------------
+z//----------------------------------------------------------------------
 // Created with uvmf_gen version 2023.4
 //----------------------------------------------------------------------
 // pragma uvmf custom header begin
@@ -72,8 +72,8 @@ class instruction_memory_transaction_coverage  extends uvm_subscriber #(.T(instr
         ignore_bins b1 = binsof(Instr_dout_bit) intersect {0} || binsof(opcode) intersect {[0:4],[6:15]};
       }
 
-    // Cross for NOT operations
-    not_cross : cross coverage_trans.src1,coverage_trans.dest
+    // Cross for NOT instructions
+    not_cross : cross coverage_trans.src1,coverage_trans.dest,opcode
       {
         ignore_bins b1 = binsof(opcode) intersect {[0:8],[10:15]};
       }
@@ -81,68 +81,67 @@ class instruction_memory_transaction_coverage  extends uvm_subscriber #(.T(instr
 
 
   covergroup memory_cg;
-    opcode: coverpoint coverage_trans.opcode;
+    opcode : coverpoint coverage_trans.opcode;
 
-    // Cross for Load PC relative instructions
-    load_PC_rel_cross : cross coverage_trans.dest,coverage_trans.PCoffset9,opcode
-    {
-      ignore_bins b2 = binsof(opcode) intersect {[0:1],[3:15]}; 
-    }
+    // Cross for load PC relative instructions
+    load_PC_rel_cross : cross coverage_trans.PCoffset9,coverage_trans.dest,opcode
+      {
+        ignore_bins b2 = binsof(opcode) intersect {[0:1],[3:15]};
+      }
 
-    // Cross for Load Register relative instructions
-    load_reg_rel_cross : cross coverage_trans.dest,coverage_trans.BaseR,coverage_trans.PCoffset6,opcode
-    {
-      ignore_bins b2 = binsof(opcode) intersect {[0:5],[7:15]}; 
-    }
+    // Cross for load Register relative instructions
+    load_reg_rel_cross : cross coverage_trans.PCoffset6,coverage_trans.dest,opcode,coverage_trans.BaseR
+      {
+        ignore_bins b2 = binsof(opcode) intersect {[0:5],[7:15]};
+      }
 
-    // Cross for Load Indirect instructions
-    load_ind_cross : cross coverage_trans.dest,coverage_trans.PCoffset9,opcode
-    {
-      ignore_bins b2 = binsof(opcode) intersect {[0:9],[11:15]}; 
-    }
-  
-    // Cross for Load Effective Address instructions
-    load_eff_addr_cross : cross coverage_trans.dest,coverage_trans.PCoffset9,opcode
-    {
-      ignore_bins b2 = binsof(opcode) intersect {[0:13],15}; 
-    }
+    // Cross for load indirect instructions
+    load_ind_cross : cross coverage_trans.PCoffset9,coverage_trans.dest,opcode
+      {
+        ignore_bins b2 = binsof(opcode) intersect {[0:9],[11:15]};
+      }
+
+    // Cross for load indirect instructions
+    load_eff_addr_cross : cross coverage_trans.PCoffset9,coverage_trans.dest,opcode
+      {
+        ignore_bins b2 = binsof(opcode) intersect {[0:13],15};
+      }
 
     // Cross for Store PC relative instructions
-    store_PC_rel_cross : cross coverage_trans.src,coverage_trans.PCoffset9,opcode
-    {
-      ignore_bins b2 = binsof(opcode) intersect {[0:2],[4:15]}; 
-    }
+    store_PC_rel_cross : cross coverage_trans.PCoffset9,coverage_trans.src,opcode
+      {
+        ignore_bins b2 = binsof(opcode) intersect {[0:2],[4:15]};
+      }
 
     // Cross for Store Register relative instructions
-    store_reg_rel_cross : cross coverage_trans.src,coverage_trans.BaseR,coverage_trans.PCoffset6,opcode
-    {
-      ignore_bins b2 = binsof(opcode) intersect {[0:6],[8:15]}; 
-    }
+    store_reg_rel_cross : cross coverage_trans.PCoffset6,coverage_trans.src,opcode,coverage_trans.BaseR
+      {
+        ignore_bins b2 = binsof(opcode) intersect {[0:6],[8:15]};
+      }
 
-     // Cross for Store Indirect instructions
-    store_ind_cross : cross coverage_trans.src,coverage_trans.PCoffset9,opcode
-    {
-      ignore_bins b2 = binsof(opcode) intersect {[0:10],[12:15]}; 
-    }
-  
+    // Cross for Store indirect instructions
+    store_ind_cross : cross coverage_trans.PCoffset9,coverage_trans.src,opcode
+      {
+        ignore_bins b2 = binsof(opcode) intersect {[0:10],[12:15]};
+      }
+
   endgroup
 
 
   covergroup control_cg;
-    opcode: coverpoint coverage_trans.opcode;
+    opcode : coverpoint coverage_trans.opcode;
 
-    //Cross for Branch Instructions
-    branch_cross : cross opcode,coverage_trans.PCoffset9,coverage_trans.cnd_flags
-    {
-      ignore_bins b3 = binsof(opcode) intersect {[1:15]};
-    }
+    // Cross for Branch instructions
+    branch_cross : cross coverage_trans.PCoffset9,coverage_trans.cnd_flags,opcode
+      {
+        ignore_bins b3 = binsof(opcode) intersect {[1:15]};
+      }
 
-    //Cross for Jump Instructions
-    jump_cross : cross opcode,coverage_trans.BaseR
-    {
-      ignore_bins b3 = binsof(opcode) intersect {[1:11],[13:15]};
-    }
-
+    // Cross for Jump instructions
+    jump_cross : cross coverage_trans.BaseR,opcode
+      {
+        ignore_bins b3 = binsof(opcode) intersect {[0:11],[13:15]};
+      }
   endgroup
 
   // ****************************************************************************
@@ -151,7 +150,10 @@ class instruction_memory_transaction_coverage  extends uvm_subscriber #(.T(instr
   //
   function new(string name="", uvm_component parent=null);
     super.new(name,parent);
-    instruction_memory_transaction_cg=new;
+    //instruction_memory_transaction_cg=new;
+    alu_cg=new;
+    memory_cg=new;
+    control_cg=new;
     `uvm_warning("COVERAGE_MODEL_REVIEW", "A covergroup has been constructed which may need review because of either generation or re-generation with merging.  Please note that transaction variables added as a result of re-generation and merging are not automatically added to the covergroup.  Remove this warning after the covergroup has been reviewed.")
   endfunction
 
@@ -160,7 +162,10 @@ class instruction_memory_transaction_coverage  extends uvm_subscriber #(.T(instr
   // This function is the standard UVM build_phase.
   //
   function void build_phase(uvm_phase phase);
-    instruction_memory_transaction_cg.set_inst_name($sformatf("instruction_memory_transaction_cg_%s",get_full_name()));
+    alu_cg.set_inst_name($sformatf("ALU_transaction_cg_%s",get_full_name()));
+    memory_cg.set_inst_name($sformatf("Memory_transaction_cg_%s",get_full_name()));
+    control_cg.set_inst_name($sformatf("Control_transaction_cg_%s",get_full_name()));
+    //instruction_memory_transaction_cg.set_inst_name($sformatf("instruction_memory_transaction_cg_%s",get_full_name()));
   endfunction
 
   // ****************************************************************************
@@ -182,4 +187,5 @@ endclass
 
 // pragma uvmf custom external begin
 // pragma uvmf custom external end
+
 
