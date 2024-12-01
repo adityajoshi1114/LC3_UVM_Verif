fb3c8431743f10269f4731f1b925453167eefe97
Aditya Joshi <ajoshi25@grendel31.ece.ncsu.edu>
2024-11-30 19:11:07 -0500

    Changed ALU sequence to get coverage faster

--- UVMF_TB/verification_ip/interface_packages/instruction_memory_pkg/src/instruction_memory_alu_instr_sequence.svh
+++ UVMF_TB/verification_ip/interface_packages/instruction_memory_pkg/src/instruction_memory_alu_instr_sequence.svh
@@ -20,64 +20,100 @@ task body();
    end
    
    //ADD 2 source registers
-   repeat(1600) begin
-      req.randomize() with {opcode == ADD;};
-      req.Instr_Dout[2:0] = req.src2;
-      req.Instr_Dout[5:3] = 3'b000;
-      req.Instr_Dout[8:6] = req.src1;
-      req.Instr_Dout[11:9] = req.dest;
-      req.Instr_Dout[15:12] = req.opcode;
+   repeat(512) begin
+      // req.randomize() with {opcode == ADD;};
+      // req.Instr_Dout[2:0] = req.src2;
+      // req.Instr_Dout[5:3] = 3'b000;
+      // req.Instr_Dout[8:6] = req.src1;
+      // req.Instr_Dout[11:9] = req.dest;
+      // req.Instr_Dout[15:12] = req.opcode;
+      req.randomize() with {Instr_Dout[15:12] == ADD; Instr_Dout[5:3] == 0;};
+      req.src2 = req.Instr_Dout[2:0];
+      req.src1 = req.Instr_Dout[8:6];
+      req.dest = req.Instr_Dout[11:9];
+      req.opcode = req.Instr_Dout[15:12];
 
       assert(req.Instr_Dout[15:12] == 4'b0001);
       start_item(req);
       finish_item(req);
    end
+
+   // One remaining instr for ADD REG (R0 -dest R3 - src1 R0 - src2)
+   req.Instr_Dout[15:12] = 4'b0001;
+   req.Instr_Dout[11:9] = 0;
+   req.Instr_Dout[8:6] = 3;
+   req.Instr_Dout[5:3] = 0;
+   req.Instr_Dout[2:0] = 0;
+   assert(req.Instr_Dout[15:12] == 4'b0001);
+   start_item(req);
+   finish_item(req);
+
    // ADD Source Register and ADD Immediate
-   repeat(4096) begin
-      req.randomize() with {opcode == ADD;};
-      req.Instr_Dout[4:0] = req.imm5;
-      req.Instr_Dout[5] = 1'b1;
-      req.Instr_Dout[8:6] = req.src1;
-      req.Instr_Dout[11:9] = req.dest;
-      req.Instr_Dout[15:12] = req.opcode;
+   repeat(2048) begin
+      // req.randomize() with {opcode == ADD;};
+      // req.Instr_Dout[4:0] = req.imm5;
+      // req.Instr_Dout[5] = 1'b1;
+      // req.Instr_Dout[8:6] = req.src1;
+      // req.Instr_Dout[11:9] = req.dest;
+      // req.Instr_Dout[15:12] = req.opcode;
+      req.randomize() with {Instr_Dout[15:12] == ADD;Instr_Dout[5] == 1'b1;};
+      req.imm5 = req.Instr_Dout[4:0];
+      req.src1 = req.Instr_Dout[8:6];
+      req.dest = req.Instr_Dout[11:9];
+      req.opcode = req.Instr_Dout[15:12];
+
 
       assert(req.Instr_Dout[15:12] == 4'b0001);
       start_item(req);
       finish_item(req);
    end
    // AND 2 source Register
-   repeat(1600) begin
-      req.randomize() with {opcode == AND;};
-      req.Instr_Dout[2:0] = req.src2;
-      req.Instr_Dout[5:3] = 3'b000;
-      req.Instr_Dout[8:6] = req.src1;
-      req.Instr_Dout[11:9] = req.dest;
-      req.Instr_Dout[15:12] = req.opcode;
+   repeat(512) begin
+      // req.randomize() with {opcode == AND;};
+      // req.Instr_Dout[2:0] = req.src2;
+      // req.Instr_Dout[5:3] = 3'b000;
+      // req.Instr_Dout[8:6] = req.src1;
+      // req.Instr_Dout[11:9] = req.dest;
+      // req.Instr_Dout[15:12] = req.opcode;
+      req.randomize() with {Instr_Dout[15:12] == AND; Instr_Dout[5:3] == 0;};
+      req.src2 = req.Instr_Dout[2:0];
+      req.src1 = req.Instr_Dout[8:6];
+      req.dest = req.Instr_Dout[11:9];
+      req.opcode = req.Instr_Dout[15:12];
 
       assert(req.Instr_Dout[15:12] == 4'b0101);
       start_item(req);
       finish_item(req);
    end
    // AND Source Register and AND Immediate
-   repeat(4096) begin
-      req.randomize() with {opcode == AND;};
-      req.Instr_Dout[4:0] = req.imm5;
-      req.Instr_Dout[5] = 1'b1;
-      req.Instr_Dout[8:6] = req.src1;
-      req.Instr_Dout[11:9] = req.dest;
-      req.Instr_Dout[15:12] = req.opcode;
+   repeat(2048) begin
+      // req.randomize() with {opcode == AND;};
+      // req.Instr_Dout[4:0] = req.imm5;
+      // req.Instr_Dout[5] = 1'b1;
+      // req.Instr_Dout[8:6] = req.src1;
+      // req.Instr_Dout[11:9] = req.dest;
+      // req.Instr_Dout[15:12] = req.opcode;
+      req.randomize() with {Instr_Dout[15:12] == AND; Instr_Dout[5] == 1'b1;};
+      req.imm5 = req.Instr_Dout[4:0];
+      req.src1 = req.Instr_Dout[8:6];
+      req.dest = req.Instr_Dout[11:9];
+      req.opcode = req.Instr_Dout[15:12];
 
       assert(req.Instr_Dout[15:12] == 4'b0101);
       start_item(req);
       finish_item(req);
    end
   // NOT operation
-   repeat(200) begin
-     req.randomize() with {opcode == NOT;};
-     req.Instr_Dout[5:0] = 6'b111111;;
-     req.Instr_Dout[8:6] = req.src1;
-     req.Instr_Dout[11:9] = req.dest;
-     req.Instr_Dout[15:12] = req.opcode;
+   repeat(64) begin
+   //   req.randomize() with {opcode == NOT;};
+   //   req.Instr_Dout[5:0] = 6'b111111;;
+   //   req.Instr_Dout[8:6] = req.src1;
+   //   req.Instr_Dout[11:9] = req.dest;
+   //   req.Instr_Dout[15:12] = req.opcode;
+      req.randomize() with {Instr_Dout[15:12] == NOT; Instr_Dout[5:0] == 6'b111111;};
+      req.src1 = req.Instr_Dout[8:6];
+      req.dest = req.Instr_Dout[11:9];
+      req.opcode = req.Instr_Dout[15:12];
 
      assert(req.Instr_Dout[15:12] == 4'b1001);
      start_item(req);
